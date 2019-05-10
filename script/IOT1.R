90bf35c99149ab48c1cc5c92a4f452db28d0528c
Borja <borjabombi@gmail.com>
2019-05-10 14:49:40 +0200

    New datasets and plotting

--- script/IOT1.R
+++ script/IOT1.R
@@ -1,5 +1,6 @@
 # Settings -----
-pacman::p_load(chrone, dplyr, RMySQL, lubridate)
+pacman::p_load(chrone, plyr, dplyr, RMySQL, lubridate, ggplot2, reshape2, 
+               quantmod, scales, RColorBrewer, sqldf, ggfortify)
 
 current_path <- getActiveDocumentContext()$path
 setwd(dirname(current_path))
@@ -38,18 +39,65 @@ df <- cbind(df,paste(df$Date,df$Time), stringsAsFactors = FALSE)
 colnames(df)[10] <- "DateTime"
 df <- df[,c(ncol(df), 1:(ncol(df)-1))]
 df$DateTime <- as.POSIXct(df$DateTime, "%Y/%m/%d %H:%M:%S")
-attr(df$DateTime, "tzone") <- "Europe/Paris"
+attr(df$DateTime, "tzone") <- "Europe"
+
+df <- df %>% mutate(Total_Power = Global_active_power + Global_reactive_power)
 
 ## Create attributes from "DateTime" ----
 
-df$year <- as.integer(year(df$DateTime))
-df$month <- as.integer(month(df$DateTime))
+df$year <- year(df$DateTime)
+df$month <- month(df$DateTime)
 df$day <- day(df$DateTime)
 df$weekday <- weekdays.POSIXt(df$DateTime)
-df$week <- as.integer(week(df$DateTime))
-df$hour <- as.integer(hour(df$DateTime))
-df$minute <- as.integer(minute(df$DateTime))
-df$quarter <- as.integer(quarter(df$DateTime))
+df$week <- week(df$DateTime)
+df$hour <- hour(df$DateTime)
+df$minute <- minute(df$DateTime)
+df$quarter <- quarter(df$DateTime)
+df$Time2 <- hms(df$Time)
+df$yearmonth <- as.yearmon(df$DateTime)
+
+# Time Series plotting by day ----
+df2 <- df %>% group_by(Date) %>% dplyr::summarise(sum_total_power = sum(Total_Power))
+df2$year <- year(df2$Date)
+df2$month <- month(df2$Date)
+df2$monthf <- factor(df2$month,levels=as.character(1:12),labels=c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"),ordered=TRUE)
+df2$week <- week(df2$Date)
+df2$yearmonth <- as.yearmon(df2$Date)
+df2$yearmonthf <- as.character(df2$yearmonth)
+df2$weekday <- as.POSIXlt(df2$Date)$wday
+df2$weekdayf <- factor(df2$weekday,levels=rev(1:7),labels=rev(c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")),ordered=TRUE)
+df2$Date <- date(df2$Date)
+
+df2 <- ddply(df2,.(yearmonthf),transform,monthweek=1+week-min(week))
+
+ggplot(df2, aes(monthweek, weekdayf, fill = sum_total_power)) + 
+  geom_tile(colour = "white") + facet_grid(year~month) + scale_fill_gradient(low="red", high="yellow") +
+  ggtitle("Total Power Consume") +  xlab("\n\nWeek of Month") + ylab("") + theme_bw()
+
+# Now by month ----
+df3 <- df2 %>% group_by(yearmonth) %>% dplyr::summarise(mean_total_power = mean(sum_total_power))
+df3$year <- year(df3$yearmonth)
+df3$month <- month(df3$yearmonth)
+df3$yearmonthf <- as.character(df3$yearmonth)
+
+ggplot(df3, aes(x = yearmonth, y = mean_total_power)) + 
+  geom_line(color = "#00AFBB", size = 1) + ylab("Whatts") +
+  xlab("") + ggtitle("Mean power consume per month")
+
+#df %>% filter(year >= 2007 & year <= 2009) %>% group_by(yearmonth) %>% 
+  #summarise(sum_global_active_power = sum(conditioning))
+
+# check if there are any time gap ----
+df$gap <- c(NA, with(df, hour[-1] - hour[-nrow(df)]))
+
+which(df$gap > 1)
+
+x1 <- df[(c((which(df$gap > 1)-1),(which(df$gap > 1)))),1]
+x1 <- as.data.frame(x1)
+
+rm(x1)
+
+df$gap <- NULL
 
 # check if there are any time gap ----
 df$gap <- c(NA, with(df, minute[-1] - minute[-nrow(df)]))
@@ -59,10 +107,17 @@ which(df$gap > 1)
 df$gap <- NULL
 
  #Power Fares----
-# Off-peak time is between 23:30 and 7:30
+# Off-peak time is between 08:00 and 12:00; and between 17:00 and 20:00
 # Off-peak price per kWh is 0,1244 €
-# Peak time is between 7:31 and 23:29
+# Peak time is between 20:00 and 08:00; and between 12:00 and 17:00
 # Peak Price per kWh is 0,1593
 normal_fare <- read.csv("dataset/NormalFares.csv")
 peak_fare <- read.csv("dataset/PeakFares.csv")
 
+# Creating a new variable for "Peak" consumes ----
+x <- as.numeric(hms(c("08:00", "12:00", "17:00", "20:00")))
+
+df$tariff <- ifelse(df$Time2 > x[1] & df$Time2 < x[2] | df$Time2 > x[3] & 
+                      df$Time2 < x[4], "off_peak", "peak")
+
+calendarheat
